0adafaa8a80f367e352ec31f2701667bfa710288
kohara <asigaru_1059@yahoo.co.jp>
2017-06-02 17:10:43 +0900

    hajiboot 2 added3

--- hajiboot-jpa/src/main/java/com/example/domain/Customer.java
+++ hajiboot-jpa/src/main/java/com/example/domain/Customer.java
@@ -1,7 +1,5 @@
 package com.example.domain;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- hajiboot-jpa/src/main/java/com/example/repository/CustomerRepository.java
+++ hajiboot-jpa/src/main/java/com/example/repository/CustomerRepository.java
@@ -1,13 +1,13 @@
 package com.example.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.domain.Customer;
 
-@Repository
-@Transactional
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-	
+	@Query("SELECT x FROM Customer x ORDER BY x.firstName, x.lastName")
+	Page<Customer> findAllOrderByName(Pageable pageable);
 }
